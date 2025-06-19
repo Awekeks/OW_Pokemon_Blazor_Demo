cef9cfb5eee25f656d9b32e17919aed4f8329a64
Daniel Pabel <dev.pabel@gmail.com>
2025-06-19 22:17:11 +0200

    added some more pokemon infos

--- Models/Pokemons/PokemonDetailsDto.cs
+++ Models/Pokemons/PokemonDetailsDto.cs
@@ -4,13 +4,15 @@ public class PokemonDetailsDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int BaseExperience { get; set; }
     public int Height { get; set; }
     public int Weight { get; set; }
     public Sprites Sprites { get; set; }
     public Cries Cries { get; set; }
+    public List<AbilitySlot> Abilities { get; set; }
+    public List<TypeSlot> Types { get; set; }
 }
 
+#region SubClasses
 public class Sprites
 {
     public string Front_Default { get; set; }
@@ -21,3 +23,29 @@ public class Cries
     public string Latest { get; set; }
     public string Legacy { get; set; }
 }
+
+public class TypeSlot
+{
+    public int Slot { get; set; }
+    public TypeInfo Type { get; set; }
+}
+
+public class TypeInfo
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
+
+public class AbilitySlot
+{
+    public AbilityInfo Ability { get; set; }
+    public bool Is_Hidden { get; set; }
+    public int Slot { get; set; }
+}
+
+public class AbilityInfo
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
+#endregion
